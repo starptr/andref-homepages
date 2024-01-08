5493adc7e22651964eea9df96b2b090c69df4849
Yuto Nishida <yuyu.nishida@gmail.com>
2024-01-07 16:49:49 -0800

    Add build script and gitignore

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i python3.11 -p python3
+
+import subprocess
+import glob
+import os
+from pathlib import Path
+
+def set_cwd_to_script_dir():
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+set_cwd_to_script_dir()
+
+os.makedirs("build", exist_ok=True)
+
+res = subprocess.run(["git", "ls-files"], check=True, capture_output=True, text=True)
+files = res.stdout.splitlines()
+
+for file in files:
+    content = None
+    with open(file, 'r') as fin:
+        content = fin.read()
+
+    output_path = Path(f'build/{file}')
+    output_path.parent.mkdir(exist_ok=True, parents=True)
+    with open(output_path, 'w') as fout:
+        fout.write(content)
